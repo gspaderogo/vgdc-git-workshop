f30e97828b82b78ea795304e799b0713f3807155
Gilbert Paderogo <gspaderogo@csu.fullerton.edu>
2018-10-10 17:18:34 -0700

    Assets
    added assets

---
+++ git_workshop/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // private by default
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // get a reference to component
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb2d.AddForce(new Vector2(10, 0));
+	}
+}
